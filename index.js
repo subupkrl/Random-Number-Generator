7257d347c9cad3fb9e92c43b59c005fc3fac0bc8
subupkrl <139966058+subupkrl@users.noreply.github.com>
2024-05-16 21:24:48 +0545

    Random Number generate

---
+++ index.js
@@ -0,0 +1,18 @@
+const module = angular.module("numberApp",[])
+
+module.controller("NumberController", function($scope){
+  $scope.getInput=function(){
+    if($scope.min>$scope.max){
+      [$scope.min,$scope.max  ]=[$scope.max,$scope.min]
+    }
+    $scope.generateNumber();
+  }
+  $scope.loadNum = function(){
+$scope.min=1;
+$scope.max=10;
+$scope.generateNumber();
+  }
+  $scope.generateNumber = function(){
+    $scope.randomNumber = Math.floor(Math.random()*($scope.max-$scope.min+1) + $scope.min)
+  }
+});
